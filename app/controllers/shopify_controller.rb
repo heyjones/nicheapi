dba7a9fa0dbdc19f4f0045d1fb7f7379fd03903d
heyjones <chris@heyjones.com>
2014-08-22 21:13:20 -0700

    began testing order create

--- app/controllers/shopify_controller.rb
+++ app/controllers/shopify_controller.rb
@@ -65,17 +65,18 @@ class ShopifyController < ApplicationController
 #  		order.line_items.each do |line_item|
 #  			products.push([line_item.sku, line_item.quantity])
 #  		end
- 		person = []
-		person[:firstName] = params[:customer][:first_name]
-		person[:lastName] = params[:customer][:last_name]
-		person[:address] = params[:shipping_address][:address1]
-		person[:postcode] = params[:shipping_address][:zip]
-		person[:suburb] = params[:shipping_address][:city]
-		person[:state] = params[:shipping_address][:province_code]
-		person[:email] = params[:email]
-		person[:phone] = params[:shipping_address][:phone]
-		person[:optInMailingList] = params[:buyer_accepts_marketing]
-		person[:countryCodeISO3166_A2] = params[:shipping_address][:country_code]
+ 		person = {
+ 			:firstName => params[:customer][:first_name]
+			:lastName = params[:customer][:last_name]
+			:address = params[:shipping_address][:address1]
+			:postcode = params[:shipping_address][:zip]
+			:suburb = params[:shipping_address][:city]
+			:state = params[:shipping_address][:province_code]
+			:email = params[:email]
+			:phone = params[:shipping_address][:phone]
+			:optInMailingList = params[:buyer_accepts_marketing]
+			:countryCodeISO3166_A2 = params[:shipping_address][:country_code]
+ 		}
 logger.info person
 # 		person['firstName'] = order.customer.first_name
 # 		person['lastName'] = order.customer.last_name
