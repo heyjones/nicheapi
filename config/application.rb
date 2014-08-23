5aa126ce37d591e2ca199e02e7f6ae4bd3d36493
heyjones <chris@heyjones.com>
2014-08-22 20:27:04 -0700

    began testing order create

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Nicheapi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+	if Rails.env.prodution?
+		config.logger = Logger.new(STDOUT)
+	end
   end
 end
