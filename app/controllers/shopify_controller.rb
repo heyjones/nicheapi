8db8191cbcd745948c2da5b7de3a1ae28fce8c8d
heyjones <chris@heyjones.com>
2014-08-22 20:59:24 -0700

    began testing order create

--- app/controllers/shopify_controller.rb
+++ app/controllers/shopify_controller.rb
@@ -62,20 +62,30 @@ class ShopifyController < ApplicationController
 	def order
 		order = params.to_hash
 		products = []
- 		params[:line_items].each do |line_item|
+ 		order.line_items.each do |line_item|
  			products.push([line_item.sku, line_item.quantity])
  		end
  		person = []
-		person['firstName'] = params[:customer][:first_name]
-		person['lastName'] = params[:customer][:last_name]
-		person['address'] = params[:shipping_address][:address1]
-		person['postcode'] = params[:shipping_address][:zip]
-		person['suburb'] = params[:shipping_address][:city]
-		person['state'] = params[:shipping_address][:province_code]
-		person['email'] = params[:email]
-		person['phone'] = params[:shipping_address][:phone]
-		person['optInMailingList'] = params[:buyer_accepts_marketing]
-		person['countryCodeISO3166_A2'] = params[:shipping_address][:country_code]
+# 		person['firstName'] = params[:customer][:first_name]
+# 		person['lastName'] = params[:customer][:last_name]
+# 		person['address'] = params[:shipping_address][:address1]
+# 		person['postcode'] = params[:shipping_address][:zip]
+# 		person['suburb'] = params[:shipping_address][:city]
+# 		person['state'] = params[:shipping_address][:province_code]
+# 		person['email'] = params[:email]
+# 		person['phone'] = params[:shipping_address][:phone]
+# 		person['optInMailingList'] = params[:buyer_accepts_marketing]
+# 		person['countryCodeISO3166_A2'] = params[:shipping_address][:country_code]
+		person['firstName'] = order.customer.first_name
+		person['lastName'] = order.customer.last_name
+		person['address'] = order.shipping_address.address1
+		person['postcode'] = order.shipping_address.zip
+		person['suburb'] = order.shipping_address.city
+		person['state'] = order.shipping_address.province_code
+		person['email'] = order.email
+		person['phone'] = order.shipping_address.phone
+		person['optInMailingList'] = order.buyer_accepts_marketing
+		person['countryCodeISO3166_A2'] = order.shipping_address.country_code
 logger.info person
 	end
 
