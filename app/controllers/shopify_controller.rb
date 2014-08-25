a89aa41ca15a02c82e2b199bc006406c7dc664b9
heyjones <chris@heyjones.com>
2014-08-24 21:19:51 -0700

    began testing order create

--- app/controllers/shopify_controller.rb
+++ app/controllers/shopify_controller.rb
@@ -77,6 +77,7 @@ class ShopifyController < ApplicationController
 			:state => params[:shipping_address][:province_code],
 			:email => params[:email],
 			:phone => params[:shipping_address][:phone],
+			:mobile => params[:shipping_address][:phone],
 			:optInMailingList => params[:buyer_accepts_marketing],
 			:countryCodeISO3166_A2 => params[:shipping_address][:country_code]
  		}
