2780144d4d34d654a304936c682c042320de40da
heyjones <chris@heyjones.com>
2014-08-29 21:37:29 -0700

    testing order code with test endpoint

--- app/models/niche.rb
+++ app/models/niche.rb
@@ -20,6 +20,6 @@ class Niche
 		login = client.call(:log_in, message: { userName: 'staff', password: 'staff' })
 		user = login.http.cookies
 		#client.call(:create_order, message: { order: order }, cookies: user)
-		client.call(:create_order, message: { order: {:products=>[{:barcode=>"1234500002120", :qty=>1}], :person=>{:firstName=>"Chris", :lastName=>"Jones", :address=>"17 Bull Run", :postcode=>"92620", :suburb=>"Irvine", :state=>"CA", :email=>"chris@heyjones.com", :phone=>"9494131049", :mobile=>"9494131049", :optInMailingList=>false, :countryCodeISO3166_A2=>"US"}, :refNo=>"TEST"} }, cookies: user)
+		client.call(:create_order, message: { order: {:products=>[{:barcode=>"1234500002120", :qty=>1}], :person=>{:firstName=>"Chris", :lastName=>"Jones", :address=>"17 Bull Run", :postcode=>"92620", :suburb=>"Irvine", :state=>"CA", :email=>"chris@heyjones.com", :phone=>"9494131049", :mobile=>"9494131049", :optInMailingList=>"false", :countryCodeISO3166_A2=>"US"}, :refNo=>"TEST"} }, cookies: user)
 	end
 end
