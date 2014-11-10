3c41d5f96c614342970a65cb83ec6bfeecba5e57
heyjones <chris@heyjones.com>
2014-11-09 18:23:40 -0800

    enable order controller

--- app/controllers/shopify_controller.rb
+++ app/controllers/shopify_controller.rb
@@ -6,53 +6,59 @@ class ShopifyController < ApplicationController
 	end
 
 	def order
-		@order = ShopifyAPI.throttle { ShopifyAPI::Order.find(params[:id]) }
-  		id = 0
-  		metafields = ShopifyAPI.throttle { order.metafields }
-  		metafields.each do |metafield|
-  			if metafield.namespace = 'nicheapi' && metafield.key = 'order'
-  				id = metafield.value
-  			end
-  		end
-  		if id == 0
-			#NICHE
-	 		person = {
-				:email => params[:email],
-	 			:firstName => params[:customer][:first_name],
-				:lastName => params[:customer][:last_name],
-				:address => params[:shipping_address][:address1],
-				:suburb => params[:shipping_address][:city],
-				:state => params[:shipping_address][:province_code],
-				:postcode => params[:shipping_address][:zip],
-				:countryCodeISO3166_A2 => params[:shipping_address][:country_code],
-				:phone => params[:shipping_address][:phone],
-				:mobile => params[:shipping_address][:phone],
-				:optInMailingList => params[:buyer_accepts_marketing].to_s
-	 		}
-	 		products = []
-	 		params[:line_items].each do |line_item|
-	 			product = {
-		 			:Barcode => line_item[:sku],
-		 			:qty => line_item[:quantity]
-	 			}
-	 			products.push(Product: product)
-	 		end
+		shopifyOrder = ShopifyAPI.throttle { ShopifyAPI::Order.find(params[:id]) }
+  		nicheId = 0
+		shopifyMetafields = ShopifyAPI.throttle { shopifyOrder.metafields }
+		shopifyMetafields.each do |shopifyMetafield|
+			if shopifyMetafield.namespace = 'nicheapi' && shopifyMetafield.key = 'order'
+				nicheId = shopifyMetafield.value
+			end
+		end
+		if nicheId == 0
+puts 'CREATE'
+			phone = shopifyOrder.shipping_address.phone
+			if phone == ''
+				phone = '1234567890'
+			end
+			person = {
+				:email => shopifyOrder.email,
+				:firstName => shopifyOrder.customer.first_name,
+				:lastName => shopifyOrder.customer.last_name,
+				:address => shopifyOrder.shipping_address.address1,
+				:suburb => shopifyOrder.shipping_address.city,
+				:state => shopifyOrder.shipping_address.province_code,
+				:postcode => shopifyOrder.shipping_address.zip,
+				:countryCodeISO3166_A2 => shopifyOrder.shipping_address.country_code,
+				:phone => phone,
+				:mobile => phone,
+				:optInMailingList => shopifyOrder.buyer_accepts_marketing.to_s
+			}
+			products = []
+			shopifyOrder.line_items.each do |line_item|
+				product = {
+					:Barcode => line_item.sku,
+					:qty => line_item.quantity
+				}
+				products.push(Product: product)
+			end
 			order = {
 				:person => person,
 				:products => products,
-				:refNo => @order.id
+				:refNo => shopifyOrder.id
 			}
-			id = Niche.order(order).to_hash[:create_order_response][:create_order_result]
+puts order
+			nicheId = Niche.order(order).to_hash[:create_order_response][:create_order_result]
 			#SHOPIFY
- 			ShopifyAPI.throttle { @order.add_metafield(ShopifyAPI::Metafield.new({
+			ShopifyAPI.throttle { shopifyOrder.add_metafield(ShopifyAPI::Metafield.new({
                  :description => '',
                  :namespace => 'nicheapi',
                  :key => 'order',
-                 :value => id,
+                 :value => nicheId,
                  :value_type => 'integer'
- 			})) }
-  		end
-		render :status => 200
+			})) }
+puts nicheId
+		end
+		render status: 200, json: order.to_json
 	end
 
 end
