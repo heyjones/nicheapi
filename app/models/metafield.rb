14875933427711d763582a903073312c433ae31f
heyjones <chris@heyjones.com>
2015-04-23 13:28:14 -0700

    touch syntax

--- app/models/metafield.rb
+++ app/models/metafield.rb
@@ -1,5 +1,5 @@
 class Metafield < ShopifyAPI::Metafield
-	belongs_to :variant, :touch => true
+	belongs_to :variant, touch: true
 	def self.cached_metafield(id)
 		Rails.cache.fetch('metafield/#{id}/#{cache_key}', expires_in: 12.hours) do
 			self.find(id)
